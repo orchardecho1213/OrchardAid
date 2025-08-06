a03c010b310d0c35c56a2a50b00b77130d7206eb
orchardecho1213 <orchardecho1213@gmail.com>
2025-08-05 20:39:49 -0400

    Delete components directory

--- components/assistant-widget.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-export default function AssistantWidget() {
-  return (
-    <div className="mt-8 p-4 border rounded text-green-800 bg-green-50">
-      <p>Hello, I'm your assistant. I'm here to help you navigate the Orchard and find peace in the digital wilderness.</p>
-    </div>
-  )
-}
