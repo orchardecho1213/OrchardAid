02e2d9cbb7a4c1758fe533814cfd114ece3f525b
orchardecho1213 <orchardecho1213@gmail.com>
2025-08-05 21:00:49 -0400

    Delete card.tsx

--- card.tsx
+++
@@ -1,6 +0,0 @@
-export function Card({ children, className = "" }) {
-  return <div className={`rounded-xl p-4 shadow ${className}`}>{children}</div>
-}
-export function CardContent({ children }) {
-  return <div className="p-4">{children}</div>
-}
