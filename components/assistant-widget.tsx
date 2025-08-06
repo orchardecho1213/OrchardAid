8543480ea19bbaa3f458d593c56c36cf173e7f31
orchardecho1213 <orchardecho1213@gmail.com>
2025-08-05 20:29:56 -0400

    Add files via upload

---
+++ components/assistant-widget.tsx
@@ -0,0 +1,8 @@
+'use client'
+export default function AssistantWidget() {
+  return (
+    <div className="mt-8 p-4 border rounded text-green-800 bg-green-50">
+      <p>Hello, I'm your assistant. I'm here to help you navigate the Orchard and find peace in the digital wilderness.</p>
+    </div>
+  )
+}
