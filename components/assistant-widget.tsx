cb4b850d073102fd2e856cd3f6a52116c46a3f1e
orchardecho1213 <orchardecho1213@gmail.com>
2025-08-06 21:47:35 -0400

    Add files via upload

--- components/assistant-widget.tsx
+++ components/assistant-widget.tsx
@@ -1,8 +1,1 @@
-'use client';
-export default function AssistantWidget() {
-  return (
-    <div className="mt-6 text-center">
-      <p className="text-sm text-gray-500">I'm Sage. I'm always here if you need to talk.</p>
-    </div>
-  );
-}
+// Assistant widget component
