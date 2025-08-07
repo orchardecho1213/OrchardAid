7a40f92735457078d080661000fe133a66a30195
orchardecho1213 <orchardecho1213@gmail.com>
2025-08-06 20:42:29 -0400

    Delete components directory

--- components/assistant-widget.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-export default function AssistantWidget() {
-  return (
-    <div className="border-t mt-12 pt-6 text-green-700">
-      <p>Assistant is standing by... (This is a placeholder AI widget)</p>
-    </div>
-  )
-}
